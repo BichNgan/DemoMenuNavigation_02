ed55188350fd5d7f4ded4b12e28978b3dd213691
BichNgan <nganinfo@gmail.com>
2024-07-12 14:14:03 +0700

    ok

--- app/src/main/java/vlu/android/demomenunavigation_02/MainActivity.java
+++ app/src/main/java/vlu/android/demomenunavigation_02/MainActivity.java
@@ -1,9 +1,13 @@
 package vlu.android.demomenunavigation_02;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu_01,menu);
+        //Hiển thị icon kèm theo cho các item
+        if(menu instanceof MenuBuilder)
+        {
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 }
